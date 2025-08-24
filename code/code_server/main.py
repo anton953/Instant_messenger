6274139a2e2350cd3a570ba1c8dc0e89b61d2302
Anton <anton.dushenkov@gmail.com>
2025-08-24 12:57:12 +0300

    refactoring

---
+++ code/code_server/main.py
@@ -0,0 +1,9 @@
+from server_function.client import *
+from server_function.server import *
+
+def main():
+    # start_server()
+    start_client()
+
+if __name__ == "__main__":
+    main()
--- code/main.py
+++
@@ -1,9 +0,0 @@
-from client import *
-from server import *
-
-def main():
-    # start_server()
-    start_client()
-
-if __name__ == "__main__":
-    main()
