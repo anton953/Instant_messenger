a91e9e90fad99f9232c3c12c8a34aff302c35fab
anton953 <anton.dushenkov@gmail.com>
2025-08-31 15:18:27 +0300

    add BL

---
+++ code/code_client/business_logic/BL.py
@@ -0,0 +1,34 @@
+from PySide6.QtWidgets import QApplication
+from PySide6.QtQml import QQmlApplicationEngine
+from PySide6.QtCore import QUrl
+import sys
+
+
+class BusinessLogic():
+    def __init__(self):
+        self.interface_init()
+        # self.main_while()
+        
+
+    def server_get_info(self):
+        pass
+
+    def interface_init(self):
+
+
+        app = QApplication(sys.argv)
+    
+        engine = QQmlApplicationEngine()
+
+        engine.load(QUrl.fromLocalFile("code/code_client/interface/QML/main_window.qml"))
+
+    
+        if not engine.rootObjects():
+            sys.exit(-1)
+    
+
+        sys.exit(app.exec())
+
+    def main_while():
+        while True:
+            pass
--- code/code_client/main.py
+++ code/code_client/main.py
@@ -1,18 +1,7 @@
-import sys
-from PySide6.QtWidgets import QApplication
-from PySide6.QtQml import QQmlApplicationEngine
-from PySide6.QtCore import QUrl
+from business_logic.BL import BusinessLogic
 
+def main():
+    logic = BusinessLogic()
 
-app = QApplication(sys.argv)
-    
-engine = QQmlApplicationEngine()
-
-engine.load(QUrl.fromLocalFile("code/code_client/interface/QML/main_window.qml"))
-
-    
-if not engine.rootObjects():
-    sys.exit(-1)
-    
 if __name__ == "__main__":
-    sys.exit(app.exec())
+    main()
